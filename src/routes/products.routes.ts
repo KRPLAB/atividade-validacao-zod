cb2ceefe3d51832f3d786791f7460d70afb6290a
krplab <kauan.r.paulino@gmail.com>
2024-03-27 20:28:44 -0300

    middleware de verificação já implementada adicionada nas rotas

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { ProductsControllers } from "../controllers/products.controllers";
 import { IsProductNameUnique } from "../middlewares/isProductNameUnique.middleware";
+import { IsProductBodyValid } from "../middlewares/isProductBodyValid.middleware";
 
 export const productsRoutes = Router();
 
 const productsControllers = new ProductsControllers();
 
-productsRoutes.post("/", IsProductNameUnique.execute, productsControllers.createProduct);
+productsRoutes.post("/", IsProductBodyValid.execute,IsProductNameUnique.execute, productsControllers.createProduct);
